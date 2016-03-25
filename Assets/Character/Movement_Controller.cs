e5a0b256929cb5a8c6c2646bf4e1602b8cbcc77a
njin19 <njin19@stanford.edu>
2016-03-25 16:50:41 -0700

    minor fix

--- Assets/Character/Movement_Controller.cs
+++ Assets/Character/Movement_Controller.cs
@@ -5,14 +5,10 @@ public class Movement_Controller : MonoBehaviour {
 
 	//sets the maximum speed
 	public float maximum_horizontal_speed = 10f;
-<<<<<<< Updated upstream
-=======
-	public float maximum_vertical_speed = 10f;
 	public float init_angle = 0f;
 	public float delta_angle = 10f; //degrees
-	public Vector2 angle_range = new Vector2(-80f, 80f);
+	public Vector2 angle_range = new Vector2(-80f, 80f); //degrees
 
->>>>>>> Stashed changes
 	private Rigidbody2D rigid_body;
 	private float angle;
 
@@ -25,12 +21,7 @@ public class Movement_Controller : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		float horizontal_input = Input.GetAxis ("Horizontal");
-<<<<<<< Updated upstream
-		float vertical_input = Input.GetAxis ("Vertical");
 		rigid_body.velocity = new Vector2 (horizontal_input * maximum_horizontal_speed,0);
-
-=======
-		rigid_body.velocity = new Vector2 (horizontal_input * maximum_horizontal_speed, 0);
 
 		if (Input.GetKeyDown ("z") && angle < angle_range[1])
 			RotateLeft ();
@@ -38,17 +29,14 @@ public class Movement_Controller : MonoBehaviour {
 			RotateRight ();
 		
 	}
-
-
+		
 	void RotateLeft() {
 		transform.Rotate (0f, 0f, delta_angle);
 		angle += delta_angle;
 	}
-
-
+		
 	void RotateRight() {
 		transform.Rotate (0f, 0f, -delta_angle);
 		angle -= delta_angle;
->>>>>>> Stashed changes
 	}
 }
